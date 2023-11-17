e34e8dfd46218b6f76422dfb9ff38946ae2402af
fildzahattira <fildzahattira560@gmail.com>
2023-11-17 21:47:30 +0700

    view login,register

--- application/views/v_login.php
+++ application/views/v_login.php
@@ -163,8 +163,8 @@
     <h1 class="h3 mb-3 fw-normal">Sign in to <b>TimelyGoodies</b></h1>
 
     <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-      <label for="floatingInput">Email address</label>
+      <input type="text" class="form-control" id="floatingInput" placeholder="name">
+      <label for="floatingInput">Username</label>
     </div>
     <div class="form-floating">
       <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
